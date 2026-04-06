86f57018b244df5bd58f736dc39627f6491bd29a
Nebat-Taha <nebatn@gmail.com>
2026-04-05 20:12:43 -0400

    ansible refactor 6

--- terraform/global/main.tf
+++ terraform/global/main.tf
@@ -1,40 +1,30 @@
-#  --- IDENTITY (IAM) ---
-module "iam_aws" {
-  source       = "../modules/iam"
-  project_name = var.project_name
-  role_name    = "${var.project_name}-ansible-aws-cicd-role"
+# 1. Attach the permissions to your MANUAL role
+resource "aws_iam_role_policy" "cicd_manual_policy" {
+  name = "ansible-aws-cicd-permissions"
+  role = "ansible-aws-cicd-role" # Points to your manual role
 
-
-  # We combine the ec2 permissions with the Instance Connect permission here.
-  policy_json = jsonencode({
+  policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
-      # Permission 1: ec2 instance
       {
-        Action = [
-          "ec2:DescribeInstances",
-          "ec2:DescribeTags",
-
-        ]
+        Action   = ["ec2:DescribeInstances", "ec2:DescribeTags"]
         Effect   = "Allow"
         Resource = "*"
       },
-      # Permission 2: Keyless SSH Access (EC2 Instance Connect)
       {
         Effect   = "Allow"
         Action   = "ec2-instance-connect:SendSSHPublicKey"
         Resource = "*"
         Condition = {
-          StringEquals = {
-            "ec2:osuser" = "ubuntu"
-          }
+          StringEquals = { "ec2:osuser" = "ubuntu" }
         }
       }
     ]
   })
 }
-# 2. The Instance Profile (WHAT YOU NEED TO ADD)
+
+# 2. Create the Instance Profile (The "Bridge")
 resource "aws_iam_instance_profile" "cicd_profile" {
-  name = "ansible-aws-cicd-role" # Give it the exact same name as the role
-  role = aws_iam_role.cicd_role.name
+  name = "ansible-aws-cicd-role"
+  role = "ansible-aws-cicd-role"
 }
